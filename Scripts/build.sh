d1d3143ec8b3c104b03434604c4a2a10b553addb
yalohi <@yalohi:matrix.org>
2021-10-29 04:01:07 +0200

    fix: app volumes not updating

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,16 +8,17 @@ if [ -z "${MY_BUILD_PATH}" ]; then exit 1; fi
 if [ -z "${MY_VOLUME_NAME_WASM_APP}" ]; then exit 1; fi
 if [ -z "${MY_VOLUME_NAME_WEBAPI_APP}" ]; then exit 1; fi
 
-docker container prune -f || exit 1
+cd ${SCRIPT_PATH}/../${MY_DOCKER_PATH}
+docker-compose rm -f || exit 1
 docker volume rm -f ${MY_VOLUME_NAME_WASM_APP} || exit 1
 docker volume rm -f ${MY_VOLUME_NAME_WEBAPI_APP} || exit 1
 
 cd ${SCRIPT_PATH}/../
-rsync -a --delete ${MY_DOCKER_PATH} ${MY_BUILD_PATH}
+rsync -a --delete ${MY_DOCKER_PATH} ${MY_BUILD_PATH} || exit 1
 
 cd ${SCRIPT_PATH}
-./build-no-docker.sh ${1} ${2}
+./build-no-docker.sh ${1} ${2} || exit 1
 
 cd ${SCRIPT_PATH}/../${MY_BUILD_PATH}
-docker-compose build
-docker system prune -f
+docker-compose build || exit 1
+docker system prune -f || exit 1
