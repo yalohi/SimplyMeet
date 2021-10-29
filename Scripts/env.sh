641f6a5fbed515556328f9341c9c0fffb5107aee
yalohi <@yalohi:matrix.org>
2021-10-29 13:57:33 +0200

    fix: standardized container and volume names

--- Scripts/env.sh
+++ Scripts/env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(dirname $(realpath "$0"))
+
 MY_DOCKER_PATH=Docker/
 MY_BUILD_PATH=Build/
 MY_BACKUPS_PATH=Backups/
@@ -7,6 +9,9 @@ MY_BACKUP_DAYS=7
 MY_BACKUP_NAME=$(date +"%Y-%m-%d-%H-%M-%S").tar
 MY_TEMP_BACKUP_PATH=/backup
 MY_DATA_PATH=/app/App_Data
-MY_CONTAINER_NAME_WEBAPI=simplymeet-webapi-1
-MY_VOLUME_NAME_WASM_APP=simplymeet_wasm-app
-MY_VOLUME_NAME_WEBAPI_APP=simplymeet_webapi-app
+
+. ${SCRIPT_PATH}/../${MY_DOCKER_PATH}/.env
+
+MY_CONTAINER_NAME_WEBAPI=${COMPOSE_PROJECT_NAME}-webapi
+MY_VOLUME_NAME_WASM_APP=${COMPOSE_PROJECT_NAME}-wasm-app
+MY_VOLUME_NAME_WEBAPI_APP=${COMPOSE_PROJECT_NAME}-webapi-app
